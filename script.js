b6ceda7c319136fb1451280a1892e1d804744de8
Naman-negi123 <155711308+Naman-negi123@users.noreply.github.com>
2025-07-30 10:44:14 +0530

    second commit

--- script.js
+++ script.js
@@ -5,3 +5,38 @@
     navLinks.classList.toggle("active");
   });
 
+// var name= 'raman';
+// let age = 15;
+// let rollNo = 101;
+// let addr = "abc, demo address";
+
+// console.log(name);
+
+// console.log(age);
+
+// let personInfo = {
+//     name: 'raman',
+//     age: 20,
+//     rollNo: 101,
+//     add: 'abc adress'
+// }
+
+// personInfo.name = 'Bahubali'
+
+// personInfo.qual = 'BCA';
+
+// console.log(personInfo);
+
+// delete personInfo.add;
+// console.log(personInfo);
+
+
+// console.log('demo' in personInfo);
+
+// let arrData = [1, 2, 3,'myname', 35, true, 34.6];
+
+// console.log(arrData);
+
+// let findInfo = arrData.indexOf('myname');
+
+// console.log(findInfo);
